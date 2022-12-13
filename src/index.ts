15d2874c3cab9a070f2d7d763ee518a32ce6c2a1
kaiquye <kaiquemendesilva82@gmail.com>
2022-12-13 14:52:04 -0300

    :fire: remove codes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import calcularPrecoPrazo from './services/preco-prazo.service'
 import viaCep from './services/validar-cep.service'
 import calcularTamanho from "./services/calcular-tamanho.service";
 
-calcularPrecoPrazo.calcPrecoPrazo('' as any)
-
 export  {
      calcularPrecoPrazo,
      viaCep,
